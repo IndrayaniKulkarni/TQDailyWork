1bb2db4ae56bbbe4eae863bae43db1f5e8f81806
IndrayaniKulkarni <indrayani.kulkarni25@gmail.com>
2023-02-07 20:29:00 +0530

    Complete work of number patterns classwork

--- Patterns/src/com/number/NumberPattern04.java
+++ Patterns/src/com/number/NumberPattern04.java
@@ -12,7 +12,18 @@ public class NumberPattern04 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		
-		for(int i=0; ;)
+		for(int i=0; i<=n; i++)
+		{
+			for(int j=(n-i); j>0; j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k=0; k<i; k++)
+			{
+				System.out.print((k+1));
+			}
+			System.out.println();
+		}
 		sc.close();
 
 	}
